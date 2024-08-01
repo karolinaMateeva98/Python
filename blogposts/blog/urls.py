7ae79906195b75164a28bb3d1265693762951234
“Karolina <karolina.mateeva@mentormate.com>
2024-08-01 18:40:32 +0300

    Comments feature added

--- blogposts/blog/urls.py
+++ blogposts/blog/urls.py
@@ -3,7 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
-    # path('user/', views.PostList.as_view(), name='user'),
-    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
-    
+    path('<slug:slug>/', views.post_detail, name='post_detail')   
 ]
